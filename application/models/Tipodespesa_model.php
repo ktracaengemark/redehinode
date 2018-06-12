6d12e37766b30b539d684b6251e606ec8c14d050
romatiservicos <ktracaengemark@gmail.com>
2018-06-12 16:33:55 -0300

    correção do erro

--- application/models/Tipodespesa_model.php
+++ application/models/Tipodespesa_model.php
@@ -200,7 +200,7 @@ class Tipodespesa_model extends CI_Model {
 					Tab_TipoDespesa AS TD
 
 				WHERE 
-					TD.Empresa = ' . $_SESSION['log']['Empresa'] . ' AND 
+
 					TD.idTab_Modulo = ' . $_SESSION['log']['idTab_Modulo'] . ' AND
 					(TD.Categoriadesp = "1" OR TD.Categoriadesp = "3")
 				ORDER BY
@@ -218,7 +218,7 @@ class Tipodespesa_model extends CI_Model {
 					Tab_TipoDespesa AS TD
 
 				WHERE 
-					TD.Empresa = ' . $_SESSION['log']['Empresa'] . ' AND 
+
 					TD.idTab_Modulo = ' . $_SESSION['log']['idTab_Modulo'] . ' AND
 					(TD.Categoriadesp = "1" OR TD.Categoriadesp = "3")
 				ORDER BY
